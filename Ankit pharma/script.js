facfbdd66c5bd0856e497ad7d5240f6c06397917
ankisharma77 <ankitt998844@gmail.com>
2026-02-26 10:03:12 +0530

    Add files via upload

---
+++ Ankit pharma/script.js
@@ -0,0 +1,16 @@
+
+  document.getElementById("prescription-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Prescription uploaded successfully!");
+  });
+
+ src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+ src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"
+
+
+
+
+
+
+
+ 
